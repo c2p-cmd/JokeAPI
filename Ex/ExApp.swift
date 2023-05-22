49931e369f3eb622400d2a7f6e1a0ff0f9ddbefa
Sharan Thakur <sharandt19@live.com>
2023-05-22 17:03:14 +0530

    Initial Commit

---
+++ Ex/ExApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExApp.swift
+//  Ex
+//
+//  Created by Sharan Thakur on 22/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ExApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
