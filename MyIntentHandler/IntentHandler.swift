0d831428640ba05bce8ddc50be1db80efaf9fe33
Sharan Thakur <sharandt19@live.com>
2023-08-14 17:53:23 +0530

    Added Search capablity

--- MyIntentHandler/IntentHandler.swift
+++ MyIntentHandler/IntentHandler.swift
@@ -8,7 +8,6 @@
 import Intents
 
 class IntentHandler: INExtension, TVShowIntentIntentHandling {
-    
     override func handler(for intent: INIntent) -> Any {
         // This is the default implementation.  If you want different objects to handle different intents,
         // you can override this and return the handler you want for that particular intent.
@@ -18,13 +17,29 @@ class IntentHandler: INExtension, TVShowIntentIntentHandling {
     
     func provideTvShowChoiceOptionsCollection(
         for intent: TVShowIntentIntent,
+        searchTerm: String?,
         with completion: @escaping (INObjectCollection<NSString>?, Error?) -> Void
     ) {
         let allShows = TVShowQuoteResponses.allShows
-        let tvShowsOptions: [NSString] = allShows.map { NSString(string: $0) }
+        
+        if let searchTerm {
+            let fileteredArray = allShows.filter { element in
+                return element.contains(searchTerm)
+            }.asNSSTring()
+            
+            completion(INObjectCollection<NSString>(items: fileteredArray), nil)
+        }
+        
+        let tvShowsOptions: [NSString] = allShows.asNSSTring()
         
         let collection = INObjectCollection<NSString>(items: tvShowsOptions)
         
         completion(collection, nil)
+    }
+}
+
+extension Array<String> {
+    func asNSSTring() -> [NSString] {
+        return self.map { NSString(string: $0) }
     }
 }
