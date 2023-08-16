a618456bd32e7b55e3b061275933661913b1b2e7
Sharan Thakur <sharandt19@live.com>
2023-08-16 16:52:11 +0530

    new intent for joke widget

--- MyIntentHandler/IntentHandler.swift
+++ MyIntentHandler/IntentHandler.swift
@@ -7,7 +7,7 @@
 
 import Intents
 
-class IntentHandler: INExtension, TVShowIntentIntentHandling {
+class IntentHandler: INExtension, TVShowIntentIntentHandling, JokeCategoryChoiceIntentHandling {
     override func handler(for intent: INIntent) -> Any {
         // This is the default implementation.  If you want different objects to handle different intents,
         // you can override this and return the handler you want for that particular intent.
@@ -28,6 +28,7 @@ class IntentHandler: INExtension, TVShowIntentIntentHandling {
             }.asNSSTring()
             
             completion(INObjectCollection<NSString>(items: fileteredArray), nil)
+            return
         }
         
         let tvShowsOptions: [NSString] = allShows.asNSSTring()
@@ -36,10 +37,39 @@ class IntentHandler: INExtension, TVShowIntentIntentHandling {
         
         completion(collection, nil)
     }
+    
+    func provideJokeCategoryOptionsCollection(
+        for intent: JokeCategoryChoiceIntent,
+        searchTerm: String?,
+        with completion: @escaping (INObjectCollection<NSString>?, Error?) -> Void
+    ) {
+        let arrayOfJokeCategories = [
+            "Any",
+            "Pun",
+            "Spooky",
+            "Christmas",
+            "Dark",
+            "Misc",
+            "Programming"
+        ]
+        
+        if let searchTerm {
+            let filteredArray = arrayOfJokeCategories.filter { $0.contains(searchTerm) }.asNSSTring()
+            
+            completion(INObjectCollection(items: filteredArray), nil)
+            return
+        }
+        
+        let nsStringArray = arrayOfJokeCategories.asNSSTring()
+        
+        completion(INObjectCollection(items: nsStringArray), nil)
+    }
 }
 
-extension Array<String> {
-    func asNSSTring() -> [NSString] {
-        return self.map { NSString(string: $0) }
+class CustomIntentError: Error {
+    let message: String
+    
+    init(_ message: String) {
+        self.message = message
     }
 }
